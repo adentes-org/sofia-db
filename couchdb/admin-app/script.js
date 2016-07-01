8766672b10d8b4afe4fe62361e26dd851259db08
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-01 15:45:09 +0200

    Update script.js

--- couchdb/admin-app/script.js
+++ couchdb/admin-app/script.js
@@ -1,11 +1,116 @@
 /* global QRCode PouchDB */
 var db = {};
+var gaugeOptions= {
+
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+
+        title: {
+            text: 'Team'
+        },
+
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#FFF'],
+                        [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#333'],
+                        [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 200,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+
+            tickPixelInterval: 30,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: 'open'
+            },
+            plotBands: [{
+                from: 0,
+                to: 120,
+                color: '#55BF3B' // green
+            }, {
+                from: 120,
+                to: 160,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 160,
+                to: 200,
+                color: '#DF5353' // red
+            }]
+        },
+
+        series: [{
+            name: 'open',
+            data: [80],
+            tooltip: {
+                valueSuffix: ' open'
+            }
+        }]
+
+};
 function formatStats(stats){
 	var ownerToShow = {
 		"username" : {max:200},
 		"echo1": {max:10}
 	}
-	return JSON.stringify(stats);
+	var html = '<div id="owners">'
+	$.each(ownerToShow, function (id, params) {
+		if(typeof stats.owner[id] === "undefined"){
+			return; //Skip if not in DB
+		}
+    		html += '<div id="container-owners-'+id+'" style="width: 300px; height: 200px; float: left">';
+    		window.setTimeout("$('#container-owners-"+id+"').highcharts("+JSON.stringify(gaugeOptions)+")",5000)
+	});
+	html += '</div>'
+	html += JSON.stringify(stats)
+	return html;
+	//return JSON.stringify(stats);
 }
 function getStats(){
 	db.fiches.allDocs({include_docs: true}).then(function(result){
